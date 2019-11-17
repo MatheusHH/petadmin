daf57a80bb23b65255d5ad161ac4eb8ca6bc2686
Matheus Henrique <matheussilva770@gmail.com>
2019-11-17 18:20:14 -0300

    fix mailer

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -1,9 +1,8 @@
 class MarketingMailer < ApplicationMailer
 	
-	class MarketingMailer &lt; ApplicationMailer
-	  def campaign(client, title, body)
-	    @body = body
-	    mail to: client.email, subject: title
-	  end
-	end
+  def campaign(client, title, body)
+    @body = body
+    mail to: client.email, subject: title
+  end
+
 end
