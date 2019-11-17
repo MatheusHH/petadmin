4631614a49efa7c835d9576629a84177c7bb4daa
Matheus Henrique <matheussilva770@gmail.com>
2019-11-17 15:30:50 -0300

    create mailer marketing and fix client and sell using dependent destroy

---
+++ app/mailers/marketing_mailer.rb
@@ -0,0 +1,9 @@
+class MarketingMailer < ApplicationMailer
+	
+	class MarketingMailer &lt; ApplicationMailer
+	  def campaign(client, title, body)
+	    @body = body
+	    mail to: client.email, subject: title
+	  end
+	end
+end
